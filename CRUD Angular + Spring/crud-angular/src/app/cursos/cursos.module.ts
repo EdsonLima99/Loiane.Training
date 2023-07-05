d26360beef4b62127f89aa4ae4d7c77312ee9119
Edson Lima <49738290+EdsonLima99@users.noreply.github.com>
2023-07-04 22:44:44 -0300

    CRUD Angular + Spring - Introdução
    CRUD Angular + Spring - Introdução

---
+++ CRUD Angular + Spring/crud-angular/src/app/cursos/cursos.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CursosRoutingModule } from './cursos-routing.module';
+import { CursosComponent } from './cursos/cursos.component';
+
+
+@NgModule({
+  declarations: [
+    CursosComponent
+  ],
+  imports: [
+    CommonModule,
+    CursosRoutingModule
+  ]
+})
+export class CursosModule { }
