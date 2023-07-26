4aaaaf735e9db05b96754f8872a82f4f1a34d67f
Edson Lima <49738290+EdsonLima99@users.noreply.github.com>
2023-07-26 20:08:49 -0300

    CRUD Angular + Spring | 35
    Validações da API com Java Bean Validation

--- CRUD Angular + Spring/crud-spring/src/main/java/com/edson/controller/CursoController.java
+++ CRUD Angular + Spring/crud-spring/src/main/java/com/edson/controller/CursoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.edson.model.Curso;
 import com.edson.repository.CursoRepository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Validated
 @RestController
 @RequestMapping("/api/cursos")
 // @AllArgsConstructor
@@ -36,7 +42,7 @@ public class CursoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Curso> buscarPorId(@PathVariable Long id) {
+    public ResponseEntity<Curso> buscarPorId(@PathVariable @NotNull @Positive Long id) {
         return cursoRepository.findById(id)
                 .map(registro -> ResponseEntity.ok().body(registro))
                 .orElse(ResponseEntity.notFound().build());
@@ -45,14 +51,14 @@ public class CursoController {
     // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Curso criar(@RequestBody Curso curso) {
+    public Curso criar(@RequestBody @Valid Curso curso) {
         // return ResponseEntity.status(HttpStatus.CREATED)
         // .body(cursoRepository.save(curso));
         return cursoRepository.save(curso);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Curso> atualizar(@PathVariable Long id, @RequestBody Curso curso) {
+    public ResponseEntity<Curso> atualizar(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Curso curso) {
         return cursoRepository.findById(id)
                 .map(registro -> {
                     registro.setNome(curso.getNome());
@@ -64,7 +70,7 @@ public class CursoController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+    public ResponseEntity<Void> excluir(@PathVariable @NotNull @Positive Long id) {
         return cursoRepository.findById(id)
                 .map(registro -> {
                     cursoRepository.deleteById(id);
--- CRUD Angular + Spring/crud-spring/src/main/java/com/edson/model/Curso.java
+++ CRUD Angular + Spring/crud-spring/src/main/java/com/edson/model/Curso.java
@@ -1,11 +1,16 @@
 package com.edson.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 // @Getter
@@ -17,12 +22,18 @@ public class Curso {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    //@JsonProperty("id")
+    // @JsonProperty("id")
     private Long id;
 
-    @Column(name = "nome", length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Back-End|Front-End")
     @Column(name = "categoria", length = 10, nullable = false)
     private String categoria;
 }
