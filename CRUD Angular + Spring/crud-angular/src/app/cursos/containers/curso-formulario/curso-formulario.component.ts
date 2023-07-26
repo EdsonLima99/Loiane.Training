ae00e78f5b8efc811c88371a270bb028f934d104
Edson Lima <49738290+EdsonLima99@users.noreply.github.com>
2023-07-25 21:55:28 -0300

    CRUD Angular + Spring | 34
    Validações de Formulário no Front-end

--- CRUD Angular + Spring/crud-angular/src/app/cursos/containers/curso-formulario/curso-formulario.component.ts
+++ CRUD Angular + Spring/crud-angular/src/app/cursos/containers/curso-formulario/curso-formulario.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
@@ -15,8 +15,11 @@ import { CursosService } from '../../services/cursos.service';
 export class CursoFormularioComponent {
   formulario = this.formBuild.group({
     id: [0],
-    nome: [''],
-    categoria: [''],
+    nome: [
+      '',
+      [Validators.required, Validators.minLength(5), Validators.maxLength(100)],
+    ],
+    categoria: ['', [Validators.required]],
   });
 
   constructor(
@@ -54,5 +57,25 @@ export class CursoFormularioComponent {
 
   private onError() {
     this.snackBar.open('Error ao salvar curso.', '', { duration: 5000 });
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.formulario.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo Obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 200;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo Inválido';
   }
 }
