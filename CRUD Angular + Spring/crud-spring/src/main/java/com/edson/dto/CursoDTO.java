fae39e21d3d44776c302424efec2f85655e52080
Edson Lima <49738290+EdsonLima99@users.noreply.github.com>
2023-07-27 21:44:03 -0300

    CRUD Angular + Spring | 41
    Padrão DTO (Data Transfer Object)

---
+++ CRUD Angular + Spring/crud-spring/src/main/java/com/edson/dto/CursoDTO.java
@@ -0,0 +1,12 @@
+package com.edson.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CursoDTO(Long id,
+        @NotBlank @NotNull @Length(min = 5, max = 100) String nome,
+        @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String categoria) {
+}
